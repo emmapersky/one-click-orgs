10329c4b18b53c68b153c9295f730d0679794f10
Jan Berkel <jan.berkel@gmail.com>
2008-12-29 23:26:05 +0100

    smtp tls

--- config/init.rb
+++ config/init.rb
@@ -22,14 +22,16 @@ Merb::BootLoader.before_app_loads do
 end
  
 Merb::BootLoader.after_app_loads do
+  require 'lib/smtp_tls'
   require 'lib/find_by_sql_hack'
   # This will get executed after your app's classes have been loaded.
 end
 
-Merb::Mailer.config = {
-  :host   => 'smtp.gmail.com',
-  :port   => '587',
-  :user   => 'info@oneclickor.gs',
-  :pass   => '25themisc3',
-  :auth   => :plain
-}
+
+# Merb::Mailer.config = {
+#   :host   => 'smtp.gmail.com',
+#   :port   => '587',
+#   :user   => 'info@oneclickor.gs',
+#   :pass   => '25themisc3',
+#   :auth   => :plain
+# }
