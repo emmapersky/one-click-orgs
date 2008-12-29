691d480d059f156083aec4af0083350148c5bb93
Emma Persky <emma.persky@gmail.com>
2008-12-29 23:22:58 +0100

    lib and mail

--- config/init.rb
+++ config/init.rb
@@ -25,3 +25,11 @@ Merb::BootLoader.after_app_loads do
   require 'lib/find_by_sql_hack'
   # This will get executed after your app's classes have been loaded.
 end
+
+Merb::Mailer.config = {
+  :host   => 'smtp.gmail.com',
+  :port   => '587',
+  :user   => 'info@oneclickor.gs',
+  :pass   => '25themisc3',
+  :auth   => :plain
+}
