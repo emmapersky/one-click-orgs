f630a7a405798764cabeaa307e5d74e329bb55f5
Jan Berkel <jan.berkel@gmail.com>
2009-03-06 01:46:58 +0000

    8 chars

--- app/controllers/reset_password.rb
+++ app/controllers/reset_password.rb
@@ -10,10 +10,9 @@ class ResetPassword < Application
       #run_later do
         chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
         new_password = ""
-        1.upto(6) { new_password << chars[rand(chars.size-1)] }
+        1.upto(8) { new_password << chars[rand(chars.size-1)] }
 
         m.password = new_password
-        Merb.logger.debug("reset password for #{email} to '#{new_password}'")
         m.save!
         mail = Merb::Mailer.new(:to => m.email, :from => 'info@oneclickor.gs', :subject => 'Your password', :text => <<-END)
         Dear #{m.name || 'member'},
@@ -26,10 +25,12 @@ class ResetPassword < Application
         oneclickor.gs
         END
         mail.deliver!
+        Merb.logger.debug("reset password for #{email} to '#{new_password}'")
+                
       #end
       redirect url(:action=>:index), :message => { :notice => "New password was sent to #{email}" }
     else
-      redirect url(:action=>:index), :message => { :error => "no such user with email: #{email}" }
+      redirect url(:action=>:index), :message => { :error => "No such user with email: #{email}" }
     end
   end
 end
