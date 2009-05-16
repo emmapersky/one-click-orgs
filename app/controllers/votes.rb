6620d77854f60c18f784d050df1f0dc661a851c7
Chris Mear <chris@feedmechocolate.com>
2009-05-16 18:46:53 +0100

    Tweak redirect back on vote casting action so that it can be used from any path.

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -4,18 +4,18 @@ class Votes < Application
   def vote_for(id, return_to)
     begin
       current_user.cast_vote(:for, id)
-      redirect url(return_to), :message => {:notice => "Vote for proposal cast"}
+      redirect return_to, :message => {:notice => "Vote for proposal cast"}
     rescue Exception => e
-      redirect url(return_to), :message => {:notice => "Error casting vote:#{e}"}      
+      redirect return_to, :message => {:notice => "Error casting vote:#{e}"}      
     end
   end
   
   def vote_against(id, return_to)
     begin
       current_user.cast_vote(:against, id)
-      redirect url(return_to), :message => {:notice => "Vote against proposal cast"}
+      redirect return_to, :message => {:notice => "Vote against proposal cast"}
     rescue
-      redirect url(return_to), :message => {:notice => "Error casting vote"}      
+      redirect return_to, :message => {:notice => "Error casting vote"}      
     end    
   end
 end 
