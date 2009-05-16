1c8eeb26d0192d001b2381aa3f776d10aba27282
Chris Mear <chris@feedmechocolate.com>
2009-05-16 18:41:39 +0100

    Remove unused default/scaffold actions.

--- app/controllers/decisions.rb
+++ app/controllers/decisions.rb
@@ -29,28 +29,6 @@ class Decisions < Application
     display @decision
   end
 
-  def new
-    only_provides :html
-    @decision = Decision.new
-    display @decision
-  end
-
-  def edit(id)
-    only_provides :html
-    @decision = Decision.get(id)
-    raise NotFound unless @decision
-    display @decision
-  end
-
-  def create(decision)
-    @decision = Decision.new(decision)
-    if @decision.save
-      redirect url('decisions'), :message => {:notice => "Proposal was successfully created"}
-    else
-      redirect url('decisions'), :message => {:error => "Proposal not created"}
-    end
-  end
-  
   def create_proposal(decision)
     @proposal = Decision.new(decision)
     @proposal.proposer_member_id = current_user.id
@@ -60,25 +38,4 @@ class Decisions < Application
       redirect url('proposals'), :message => {:notice => "Proposal not created"}      
     end
   end
-
-  def update(id, decision)
-    @decision = Decision.get(id)
-    raise NotFound unless @decision
-    if @decision.update_attributes(decision)
-       redirect resource(@decision)
-    else
-      display @decision, :edit
-    end
-  end
-
-  def destroy(id)
-    @decision = Decision.get(id)
-    raise NotFound unless @decision
-    if @decision.destroy
-      redirect resource(:decisions)
-    else
-      raise InternalServerError
-    end
-  end
-
 end # Decisions
