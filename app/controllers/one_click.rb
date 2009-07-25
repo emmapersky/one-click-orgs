57f343d902a2db95c732d8f3859d73d555173958
Chris Mear <chris@feedmechocolate.com>
2009-07-25 13:35:54 +0100

    Add form for proposing a change to the membership voting system.

--- app/controllers/one_click.rb
+++ app/controllers/one_click.rb
@@ -110,7 +110,6 @@ class OneClick < Application
   end
   
   def propose_amendment
-  
     if params['general_voting_system']
       proposed_system = VotingSystems.get(params['general_voting_system'])      
       current_system = Constitution.voting_system :general
@@ -124,9 +123,28 @@ class OneClick < Application
         )
 
         if proposal.save
-          redirect url(:controller=>'one_click', :action=>'control_centre'), :message => {:notice=> "Change voting system proposal successfully created"}
+          redirect url(:controller=>'one_click', :action=>'control_centre'), :message => {:notice=> "Change general voting system proposal successfully created"}
         else
           redirect '/constitution', :message => {:error => "Error creating proposal: #{proposal.errors.inspect}"}      
+        end
+        
+        return
+      end
+    elsif params['membership_voting_system']
+      proposed_system = VotingSystems.get(params['membership_voting_system'])
+      current_system = Constitution.voting_system :membership
+      
+      if current_system != proposed_system
+        proposal = ChangeVotingSystemProposal.new(
+          :title => "change membership voting system to #{proposed_system.description}",
+          :proposer_member_id => current_user.id,
+          :parameters => ChangeVotingSystemProposal.serialize_parameters('type' => 'membership', 'proposed_system' => proposed_system.simple_name)
+        )
+        
+        if proposal.save
+          redirect url(:controller=>'one_click', :action=>'control_centre'), :message => {:notice=> "Change membership voting system proposal successfully created"}
+        else
+          redirect '/constitution', :message => {:error => "Error creating proposal: #{proposal.errors.inspect}"}
         end
         
         return
--- spec/requests/one_click_spec.rb
+++ spec/requests/one_click_spec.rb
@@ -8,24 +8,49 @@ describe "/one_click" do
   end
   
   
-  describe "propose amendment" do
+  describe "proposing voting system amendments" do
     before do
       login
-      @general_voting_system = Clause.create!(:name=>'general_voting_system', :text_value=>'RelativeMajority')        
+      @general_voting_system = Clause.create!(:name=>'general_voting_system', :text_value=>'RelativeMajority')
+      @membership_voting_system = Clause.create!(:name => 'membership_voting_system', :text_value => 'RelativeMajority')
+      @constitution_voting_system = Clause.create!(:name => 'constitution_voting_system', :text_value => 'RelativeMajority')
     end
     
-    
-    it "should add a proposal" do      
-      @response = request(url(:controller=>'one_click', 
-        :action=>'propose_amendment'),
-        :method=>'POST', :params=>{:general_voting_system=>'Unanimous'})
+    describe "for general decisions" do
+      it "should add the proposal" do
+        @response = request(url(:controller=>'one_click', 
+          :action=>'propose_amendment'),
+          :method=>'POST', :params=>{:general_voting_system=>'Unanimous'})
 
-      puts @response.body if @response.status != 302
+        puts @response.body if @response.status != 302
+
+        @response.should redirect_to("/one_click/control_centre")
       
-      @response.should redirect_to("/one_click/control_centre")
-      
-      ChangeVotingSystemProposal.count.should == 1      
-      ChangeVotingSystemProposal.all.first.title.should == 'change general voting system to Supporting votes from every single member'
+        ChangeVotingSystemProposal.count.should == 1
+        ChangeVotingSystemProposal.all.first.title.should == 'change general voting system to Supporting votes from every single member'
+        proposal_parameters = YAML.JSON(ChangeVotingSystemProposal.all.first.parameters)
+        proposal_parameters['type'].should == 'general'
+        proposal_parameters['proposed_system'].should == 'Unanimous'
+      end
     end
+    
+    describe "for membership decisions" do
+      it "should add the proposal" do
+        @response = request(url(:controller=>'one_click', 
+          :action=>'propose_amendment'),
+          :method=>'POST', :params=>{:membership_voting_system=>'Veto'})
+
+        puts @response.body if @response.status != 302
+
+        @response.should redirect_to("/one_click/control_centre")
+      
+        ChangeVotingSystemProposal.count.should == 1
+        ChangeVotingSystemProposal.all.first.title.should == 'change membership voting system to No opposing votes'
+        proposal_parameters = YAML.JSON(ChangeVotingSystemProposal.all.first.parameters)
+        proposal_parameters['type'].should == 'membership'
+        proposal_parameters['proposed_system'].should == 'Veto'
+      end
+    end
+    
   end
 end
