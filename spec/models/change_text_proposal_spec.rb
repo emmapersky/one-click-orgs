0a93b09d4f8e3d63f4648bd77ef551f678b160c0
Chris Mear <chris@feedmechocolate.com>
2009-07-25 16:21:21 +0100

    Add ChangeTextProposal model.

---
+++ spec/models/change_text_proposal_spec.rb
@@ -0,0 +1,16 @@
+require File.join( File.dirname(__FILE__), '..', "spec_helper" )
+
+describe ChangeTextProposal do
+  before(:each) do
+    @objectives = Clause.create!(:name => 'objectives', :text_value => "eat all the cheese")
+  end
+  
+  it "should change the objectives after successful proposal" do
+    @p = ChangeTextProposal.new(:parameters=>{'name'=>'objectives', 'value'=>'make all the yoghurt'}.to_json)
+    @p.stub!(:passed?).and_return(true)
+    lambda {
+      @p.enact!
+    }.should change(Clause, :count).by(1)
+    Clause.get_current('objectives').text_value.should == 'make all the yoghurt'
+  end
+end
