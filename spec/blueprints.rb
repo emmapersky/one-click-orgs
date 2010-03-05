47c81001f39d208a6c997a6ea66082bed17137bd
Chris Mear <chris@feedmechocolate.com>
2010-03-05 16:02:39 +0000

    Fix some machinist errors by deferring shamming.

--- spec/blueprints.rb
+++ spec/blueprints.rb
@@ -8,7 +8,7 @@ Sham.password { Faker::Name.first_name }
 
 Member.blueprint do
   email "foo@example.com"
-  name Sham.name
+  name {Sham.name}
   created_at Time.now - 1.day
   pw = Sham.password
   password pw
@@ -23,17 +23,17 @@ Proposal.blueprint do |bp|
 end
 
 Decision.blueprint do |d|
-  d.proposal Proposal.make
+  d.proposal {Proposal.make}
 end
 
 AddMemberProposal.blueprint do |bp|
   bp.title "a proposal title"
   bp.open "1"
-  bp.proposer Member.make
+  bp.proposer {Member.make}
 end
 
 Clause.blueprint do |bp|
   bp.name 'objectives'
   bp.text_value 'consuming doughnuts'
-  bp.started_at(Time.now - 1.day)
+  bp.started_at {Time.now - 1.day}
 end
