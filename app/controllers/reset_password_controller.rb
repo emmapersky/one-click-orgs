bd9388a07bdaa72f1a55bb6c69d034d3076c22ea
Chris Mear <chris@feedmechocolate.com>
2010-03-05 22:57:43 +0000

    Fix some Ruby syntax errors in controllers.

--- app/controllers/reset_password_controller.rb
+++ app/controllers/reset_password_controller.rb
@@ -15,14 +15,14 @@ class ResetPasswordController < Application
           async_job :do_reset_email, m.id, new_password
           
           Rails.logger.debug("reset password for #{email} to '#{new_password}'")
-          redirect_to {:action=>:index}, :flash => { :notice => "New password was sent to #{email}" }                
+          redirect_to({:action=>:index}, :flash => { :notice => "New password was sent to #{email}" })
         else
           Rails.logger.debug("error resetting password: #{m.errors.inspect}")
-          redirect_to {:action=>:index}, :flash => { :error => "error resetting password: #{m.errors.inspect}" }                
+          redirect_to({:action=>:index}, :flash => { :error => "error resetting password: #{m.errors.inspect}" })                
         end
 
     else
-      redirect_to {:action=>:index}, :flash => { :error => "No such user with email: #{email}" }
+      redirect_to({:action=>:index}, :flash => { :error => "No such user with email: #{email}" })
     end
   end
   
