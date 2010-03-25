fc6fb78403bd84480dea74435955c950d86c423d
Chris Mear <chris@feedmechocolate.com>
2010-03-25 17:46:38 +0000

    Merb to Rails syntax changes missed from merge commit d4d1e250.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
-class WelcomeController < Application
+class WelcomeController < ApplicationController
   skip_before_filter :ensure_member_inducted
 
   def index
-    @organisation_name = Organisation.name
+    @organisation_name = Organisation.organisation_name
     prepare_constitution_view
   end
 
@@ -14,6 +14,6 @@ class WelcomeController < Application
   end
 
   def cancel_membership
-    @organisation_name = Organisation.name
+    @organisation_name = Organisation.organisation_name
   end
 end
