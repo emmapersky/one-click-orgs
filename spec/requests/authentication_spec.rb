24a8583ee30f2f1113b261afe14b06c8cf1d3d8f
Chris Mear <chris@feedmechocolate.com>
2010-03-27 08:29:34 +0000

    Add an 'authentication' request (integration) spec, instead of a unit test for MemberSessionsController.
    Couldn't get 'session' to work properly in the controller unit test; seems to work fine in an integration test.

---
+++ spec/requests/authentication_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+describe "authentication" do
+  before(:each) do
+    default_user
+  end
+  
+  describe "getting login form" do
+    it "should display a login form" do
+      get '/login'
+      response.should render_template('member_sessions/new')
+    end
+  end
+  
+  describe "logging in" do
+    before(:each) do
+      post member_session_path, :email => 'krusty@clown.com', :password => 'password'
+    end
+    
+    it "should log in the user" do
+      session[:user].should == (Member.last.id)
+    end
+    
+    it "should redirect to the home page" do
+      response.should redirect_to('/')
+    end
+  end
+  
+  describe "trying to log in with bad credentials" do
+    before(:each) do
+      post member_session_path, :email => 'krusty@clown.com', :password => 'wrongpassword'
+    end
+    
+    it "should not log in the user" do
+      session[:user].should be_nil
+    end
+    
+    it "should render the login page" do
+      response.should render_template('member_sessions/new')
+    end
+    
+    it "should set an error flash" do
+      flash[:error].should_not be_blank
+    end
+  end
+  
+  describe "logging out" do
+    before(:each) do
+      post member_session_path, :email => 'krusty@clown.com', :password => 'password'
+      delete member_session_path
+    end
+    
+    it "should log out the user" do
+      session[:user].should be_nil
+    end
+    
+    it "should redirect to the home page" do
+      response.should redirect_to('/')
+    end
+  end
+end
