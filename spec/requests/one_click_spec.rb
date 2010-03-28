dcc882f504aae0cd8eb3dc3d4e237bbecc88c5af
Chris Mear <chris@feedmechocolate.com>
2010-03-28 14:01:33 +0100

    Fix broken links to proposal sub-classes in Control Centre. Closes #89.

--- spec/requests/one_click_spec.rb
+++ spec/requests/one_click_spec.rb
@@ -149,4 +149,31 @@ describe "/one_click" do
       @response.should have_xpath("//table[@class='timeline']")
     end
   end
+  
+  describe "control centre" do
+    before(:each) do
+      login
+    end
+    
+    describe "with an 'add member' proposal recently decided" do
+      before(:each) do
+        @proposal = AddMemberProposal.create!(
+          :title => "Add new member",
+          :proposer_member_id => default_user.id,
+          :parameters => AddMemberProposal.serialize_parameters(:email => "new@example.com", :name => "James Nouveau")
+        )
+        @proposal.open = false
+        @proposal.close_date = Time.now.utc
+        @proposal.accepted = true
+        @proposal.save!
+        Decision.create!(:proposal_id => @proposal.id)
+      end
+      
+      # GH-89
+      it "should display a link to the recently-closed 'add member' proposal" do
+        get url_for(:controller => 'one_click', :action => 'control_centre')
+        response.should have_selector("table.decisions a[href='/proposals/#{@proposal.id}']")
+      end
+    end
+  end
 end
