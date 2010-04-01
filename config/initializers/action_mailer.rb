ab7532ba5ffe2f24c3c39f292da70bb71ddb99f0
Chris Mear <chris@feedmechocolate.com>
2010-04-01 01:17:59 +0100

    Consolidate local settings into one gitignored file. Closes #71, #73 and #74.

--- config/initializers/action_mailer.rb
+++
@@ -1,9 +0,0 @@
-OneClickOrgs::Application.configure do
-  config.action_mailer.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :port           => '587',
-    :user_name      => 'info@oneclickor.gs',
-    :password       => 'XXXX',
-    :authentication => :plain
-  }
-end
