2dd091bc06b71f7a6b48747d2915e56d9ff6e23d
Chris Mear <chris@feedmechocolate.com>
2010-04-01 01:07:12 +0100

    Quick fix for unnecessary HTML-escaping in text email templates. Closes #79.
    This needs a systemic fix, really (possibly a fix in Rails itself), but will do for now.

---
+++ spec/views/proposal_mailer/notify_creation.text.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "proposal_mailer/notify_creation.text.erb" do
+  before(:each) do
+    @member = mock_model(Member, :name => "Bob Smith")
+    assigns[:member] = @member
+    
+    @proposal = mock_model(Proposal, :title => "Buy more yaks", :description => "We're running low & they're cheap.", :to_param => '1', :proposer => mock_model(Member, :name => "Emma Baker"))
+    assigns[:proposal] = @proposal
+  end
+  
+  it "should not HTML-escape the proposal details" do
+    render
+    response.should contain "We're running low & they're cheap."
+  end
+end
