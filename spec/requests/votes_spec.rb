a695fa0202ec70cf8526045657ca1aba19801962
Chris Mear <chris@feedmechocolate.com>
2010-04-02 02:04:46 +0100

    Fully test votes requests.

--- spec/requests/votes_spec.rb
+++ spec/requests/votes_spec.rb
@@ -1,25 +1,22 @@
 require 'spec_helper'
 
 describe "everything" do
-  before do 
-    @user = login 
+  before(:each) do 
+    @user = login
+    @proposal = Proposal.make
   end
       
   describe "vote casting" do
     it "should cast a 'for' vote" do
-      #FIXME
-      #@user.should_receive(:cast_vote).with(:for, 1)
-      
-      post('/votes/vote_for/1', {:return_to => '/foo'})
-      response.should redirect_to('/foo')
+      post(url_for(:controller => 'votes', :action => 'vote_for', :id => @proposal.id), {:return_to => '/foo'})
+      response.should redirect_to '/foo'
+      @proposal.vote_by(@user).for?.should be_true
     end
   
     it "should cast an 'against' vote" do
-      #FIXME
-      #@user.should_receive(:cast_vote).with(:against, 1)
-      
-      post(url_for(:controller => 'votes', :action => 'vote_against', :id => 1), {:return_to => '/foo'})
+      post(url_for(:controller => 'votes', :action => 'vote_against', :id => @proposal.id), {:return_to => '/foo'})
       response.should redirect_to '/foo'
+      @proposal.vote_by(@user).for?.should be_false
     end
   end
 end
