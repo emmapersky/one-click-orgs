c873545e27a438a943370ccc2c08f0065b2e900b
Chris Mear <chris@feedmechocolate.com>
2010-04-02 01:22:15 +0100

    Tidy up induction spec.

--- spec/requests/induction_spec.rb
+++ spec/requests/induction_spec.rb
@@ -1,59 +1,58 @@
 require 'spec_helper'
 
-describe "/induction" do
-  
-  describe "initial creation" do
-    before do
-      organisation_is_under_construction
-    end
-    
-    it "should allow creation of founding member if no members exist and organisation is under contruction" do
-      Organisation.stub!(:has_founding_member?).and_return(false)
+describe "induction" do
+  describe "state guarding" do
+    describe "when organisation is under construction" do
+      before do
+        organisation_is_under_construction
+      end
       
-      get url_for(:controller=>'induction', :action=>'founder')
-      @response.should be_successful
-    end
-  
-    it "should always redirect to the create founder page if organisation under construction and no founding members present in the system" do
-      Organisation.stub!(:has_founding_member?).and_return(false)
+      it "should allow creation of founding member if no members exist and organisation is under contruction" do
+        Organisation.stub!(:has_founding_member?).and_return(false)
+        
+        get url_for(:controller=>'induction', :action=>'founder')
+        @response.should be_successful
+      end
       
-      get('/one_click')
-      @response.should redirect_to(:controller=>'induction', :action=>'founder')
-    end
-  end
-  
-  describe "pending state (constitution has been setup, emails have been sent)" do
-    before do
-      login      
-      organisation_is_pending
+      it "should always redirect to the create founder page if organisation under construction and no founding members present in the system" do
+        Organisation.stub!(:has_founding_member?).and_return(false)
+        
+        get('/one_click')
+        @response.should redirect_to(:controller=>'induction', :action=>'founder')
+      end
     end
     
-    it "should always redirect to founding meeting page if organisation is in pending state" do    
-      get('/one_click')
-      @response.should redirect_to(:controller=>'induction', :action=> 'founding_meeting')
-    end      
-  end
-  
-  
-  describe "active state (founding meeting has been confirmed)" do
-    
-    before do
-      organisation_is_active
-    end
-    
-    it "should require login if organisation is active and not logged in" do
-      Organisation.stub!(:has_founding_member?).and_return(true)      
-      get('/one_click')
-      response.should redirect_to(login_path)
-    end
-    
-    it "should redirect to control centre if organisation is active and any induction action is requested" do
-      login
+    describe "pending state (constitution has been setup, emails have been sent)" do
+      before do
+        login
+        organisation_is_pending
+      end
       
-      (InductionController::CONSTRUCTION_ACTIONS + InductionController::PENDING_ACTIONS).each do |a|
-        get url_for(:controller=>'induction', :action=>a)
-        @response.should redirect_to(:controller=>'one_click', :action=>'control_centre')
-      end      
+      it "should always redirect to founding meeting page if organisation is in pending state" do
+        get('/one_click')
+        @response.should redirect_to(:controller=>'induction', :action=> 'founding_meeting')
+      end
+    end
+    
+    describe "when organisation is active" do
+      before do
+        organisation_is_active
+      end
+      
+      it "should require login if organisation is active and not logged in" do
+        Organisation.stub!(:has_founding_member?).and_return(true)      
+        get('/one_click')
+        response.should redirect_to(login_path)
+      end
+      
+      it "should redirect to control centre if organisation is active and any induction action is requested" do
+        login
+        
+        (InductionController::CONSTRUCTION_ACTIONS + InductionController::PENDING_ACTIONS).each do |a|
+          get url_for(:controller=>'induction', :action=>a)
+          @response.should redirect_to(:controller=>'one_click', :action=>'control_centre')
+        end
+      end
     end
   end
   
