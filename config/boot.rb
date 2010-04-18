96aef912b2dd50b8741448d0d31ee1cdb06130fb
Jan Berkel <jan.berkel@gmail.com>
2010-04-19 01:09:57 +0200

    Use locked environment first

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
-# Set up gems listed in the Gemfile.
-if File.exist?(File.expand_path('../../Gemfile', __FILE__))
+environment = File.expand_path("../../.bundle/environment.rb", __FILE__)
+if File.exists?(environment)
+  # locked env
+  require environment
+elsif File.exist?(File.expand_path('../../Gemfile', __FILE__))
+  # Set up gems listed in the Gemfile.
   require 'bundler'
   Bundler.setup
 end
