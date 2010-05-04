c08fd587d05dfb6cffb911828bde4016defbbda2
Chris Mear <chris@feedmechocolate.com>
2010-05-04 16:28:35 +0100

    Fix that 404 page was rendered inside the normal application layout.

---
+++ spec/requests/miscellaneous_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "miscellaneous requests" do
+  describe "internally-generated 404" do
+    before(:each) do
+      login
+      get '/proposals/9999999'
+    end
+    
+    it "should render the public/404.html file" do
+      response.body.should =~ /The page you were looking for doesn't exist\./
+    end
+    
+    it "should not render using the application layout" do
+      response.should_not have_selector "div.control_bar"
+    end
+  end
+end
