6aba90b4ea8215d081d7feee24cf6f1d0a4561c2
Chris <chris@feedmechocolate.com>
2010-07-21 22:40:28 +0100

    Add confirmation dialog to 'found organisation' button. Closes gh-51.

--- spec/requests/induction_spec.rb
+++ spec/requests/induction_spec.rb
@@ -44,7 +44,9 @@ describe "induction process" do
     
     get('/induction/confirm_agenda')
     follow_redirect!
-    response.should have_selector("form[action='/induction/confirm_founding_meeting']")
+    response.should have_selector("form[action='/induction/confirm_founding_meeting']") do |form|
+      form.should have_selector("input[type=submit][data-confirm]")
+    end
     
     post '/induction/confirm_founding_meeting', :members => {second_member_id.to_s => '1'}
     response.should redirect_to '/one_click/control_centre'
