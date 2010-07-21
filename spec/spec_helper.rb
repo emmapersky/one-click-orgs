60e4949458dbf453fd74aa0f7fc7d0f02f28ce66
Chris <chris@feedmechocolate.com>
2010-07-21 21:24:47 +0100

    Upgrade to rspec-rails-2.0.0.beta.17.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,14 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
-require File.dirname(__FILE__) + "/../config/environment" unless defined?(Rails)
+require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
-Rspec.configure do |config|
+RSpec.configure do |config|
   # == Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -24,7 +24,7 @@ Rspec.configure do |config|
   # examples within a transaction, comment the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-  
+
   # Sham setup and teardown
   config.before(:all)  { Sham.reset(:before_all) }
   config.before(:each) { Sham.reset(:before_each) }
