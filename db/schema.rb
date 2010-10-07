8c1b7c5f76d6665bb1d93dcaf7e6ff3a3eaae02b
Chris Mear <chris@feedmechocolate.com>
2010-10-07 11:08:32 +0100

    Automatic DB schema update.

--- db/schema.rb
+++ db/schema.rb
@@ -1,8 +1,8 @@
-# This file is auto-generated from the current state of the database. Instead 
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your 
+# Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
@@ -52,9 +52,9 @@ ActiveRecord::Schema.define(:version => 20100820233934) do
     t.integer  "active",           :limit => 1,  :default => 1
     t.string   "crypted_password", :limit => 50
     t.string   "salt",             :limit => 50
+    t.datetime "inducted_at"
     t.integer  "organisation_id"
     t.integer  "member_class_id"
-    t.datetime "inducted_at"
   end
 
   create_table "organisations", :force => true do |t|
