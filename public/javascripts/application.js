00ceb448a71dff17c8b814eddf842ba2593c3b60
Alex Muller <alexlmuller@gmail.com>
2010-12-04 16:10:19 +0000

    First go at main page slidey forms

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,4 +10,26 @@ $(document).ready(function () {
     $(event.target).parents('.constitution_proposal').children('.proposal_form').slideUp();
     event.preventDefault();
   });
+  //
+  // Hide the forms at the top of the main page
+  // (If JavaScript is disabled, they all show by default)
+  $('.form-to-hide').hide();
+  $('.button-form-show').show();
+  // Reset when a button is clicked
+  $('.button-form-show').click(function() {
+    $('.button-form-show').removeClass('clicked');
+    $('.form-to-hide').hide();
+  });
+  // Each of the three buttons:
+  $('#button-proposal').click(function() {
+    $('#propose_freeform_form').show('medium');
+    $(this).addClass('clicked');
+  });
+  $('#button-member-invite').click(function() {
+    $('#propose_new_member_form').show('medium');
+    $(this).addClass('clicked');
+  })
+  $('#button-constitution-change').click(function() {
+    $(location).attr('href','/settings');
+  });
 });
